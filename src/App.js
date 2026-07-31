18e6f2da37ff5983fb2bbdb40d5cbb9f9f3e1b1f
vikas koli <123049251+vikas-koli@users.noreply.github.com>
2026-07-31 18:39:39 +0400

    Update route for Dashboard with AdminLayout

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ return (
 
 <BrowserRouter>
 <Routes>
-<Route path="/" element={ <Dashboard/> }/> 
-<Route path="/dashboard" element={<AdminLayout> <Dashboard/> </AdminLayout> }/> 
+<Route path="/" element={<AdminLayout> <Dashboard/> </AdminLayout> }/> 
 <Route path="/leads" element={ <AdminLayout> <LeadsPage/> </AdminLayout> } />
 <Route path="/message" element={ <AdminLayout> <MessageComposer/> </AdminLayout> } />
 </Routes>
